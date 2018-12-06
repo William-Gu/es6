b5a9dc03f51bf25a5b66c8fca0213b20984f8429
langmanqk <William@192.168.0.103>
2018-12-06 22:22:26 +0800

    a

---
+++ zepto_jquery/jQuery.js
@@ -0,0 +1,30 @@
+(function(window){
+  var jQuery = function(selector){
+    return new jQuery.fn.init(selector)
+  }
+  
+  jQuery.fn = {}
+
+  var init = jQuery.fn.init = function(selector){
+    var slice = Array.prototype.slice
+    var dom = slice.call(document.querySelectorAll(selector))
+  
+    var i, len = dom ? dom.length : 0
+    for (i = 0; i < len; i++){
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
+  }
+  
+  jQuery.fn = jQuery.prototype = {
+    constructor: jQuery,
+    css: function (key, value){
+  
+    },
+    html: function(value){
+  
+    }
+  }
+  window.$ = jQuery
+})(window)
